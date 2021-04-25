4eb58b33b34829a47a811e057193dfc86e118803
Ryu Sato <ryu@weseek.co.jp>
2021-04-26 01:30:27 +0900

    UserとPhotoを関連付けた

--- app_a/app/models/photo.rb
+++ app_a/app/models/photo.rb
@@ -1,3 +1,4 @@
 class Photo < ApplicationRecord
   has_one_attached :attachment
+  belongs_to :user
 end
--- app_a/db/schema.rb
+++ app_a/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_184833) do
+ActiveRecord::Schema.define(version: 2021_04_25_092642) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2021_04_18_184833) do
   create_table "photos", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,4 +56,5 @@ ActiveRecord::Schema.define(version: 2021_04_18_184833) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "photos", "users"
 end
