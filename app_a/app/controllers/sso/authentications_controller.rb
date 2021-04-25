58023f68bbea4aa2fba191bbbd42b9e3c6adfb61
Ryu Sato <ryu@weseek.co.jp>
2021-04-25 23:32:51 +0900

    app_aの認証・認可ルーティングをsso配下へ移動した

---
+++ app_a/app/controllers/sso/authentications_controller.rb
@@ -0,0 +1,34 @@
+module Sso
+  class AuthenticationsController < ApplicationController
+
+    def new
+      login_request = HydraService.instance.login_request(current_login_challenge)
+      accept_login_and_redirect(login_request.subject) and return if login_request.skip
+
+      @login_challenge = current_login_challenge
+    end
+
+    def create
+      user = User.find_by(email: authentication_params[:email])
+      raise Errors::AuthenticationFailed if user.blank?
+      raise Errors::AuthenticationFailed unless user.authenticate(authentication_params[:password])
+
+      accept_login_and_redirect(user.email)
+    end
+
+    private
+    
+    def accept_login_and_redirect(subject)
+      completed_request = HydraService.instance.accept_login(current_login_challenge, subject)
+      redirect_to completed_request.redirect_to
+    end
+
+    def authentication_params
+      params.permit(:email, :password)
+    end
+
+    def current_login_challenge
+      params.permit(:login_challenge).fetch(:login_challenge, {})
+    end
+  end
+end
--- app_a/config/routes.rb
+++ app_a/config/routes.rb
@@ -1,18 +1,24 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  get 'login', to: 'authentications#new'
-  get 'logout', to: 'authentications#logout'
-  get 'consent', to: 'authorizations#new'
-  post 'consent', to: 'authorizations#create'
-  resources :authentications, only: %i[new create destroy]
-  resources :authorizations, only: %i[new create]
-
   root 'photos#index'
+
+  # resources
   resources :photos
 
+  # API
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :photos, only: %i[index show]
     end
+  end
+
+  # OpenID Connect ID Provider
+  namespace :sso do
+    get 'login', to: 'authentications#new'
+    # delete 'logout', to: 'authentications#destroy'
+    resources :authentications, only: %i[create]
+
+    get 'consent', to: 'authorizations#new'
+    post 'consent', to: 'authorizations#create'
   end
 end
