4dc134211718422a7dab7b7da306480656b1a0a7
Ryu Sato <ryu@weseek.co.jp>
2021-05-03 20:24:16 +0900

    use oauth2_proxy binary. And set config propely.

--- oauth2-proxy/oauth2_proxy.sh
+++ oauth2-proxy/oauth2_proxy.sh
@@ -1,14 +1,12 @@
-# docker run -d -p 4180:4180 quay.io/oauth2-proxy/oauth2-proxy \
-docker run -d oauth2-proxy \
-  --'http-address=http://0.0.0.0:4180' \
-  --'provider=oidc' \
-  --'login-url=http://localhost:4444/oauth2/auth?response_type=code&client_id=members_application&scope=openid+members&redirect_uri=http%3A%2F%member%3A4180%2Foauth2%2Fcallback&state=ZGHWfNTjeY' \
-  --'oidc-issuer-url=http://localhost:4444/' \
-  --'oidc-jwks-url=http://localhost:4455/.well-known/jwks.json' \
-  --'client-id=members_application' \
-  --'client-secret=secret' \
-  --'email-domain=*' \
-  --'cookie-secret=hu9XrailLtAYwQCHyQkGUw==' \
-  --'cookie-secure=false' \
-#   --'upstream=http://localhost:3300/' \
-
+oauth2-proxy \
+  --upstream 'http://localhost:3300/' \
+  --provider 'oidc' \
+  --oidc-issuer-url 'http://localhost:4444/' \
+  --oidc-jwks-url 'http://localhost:4455/.well-known/jwks.json' \
+  --client-id 'members_application' \
+  --client-secret 'secret' \
+  --email-domain '*' \
+  --cookie-secret 'hu9XrailLtAYwQCHyQkGUw==' \
+  --cookie-secure 'false' \
+  --redirect-url 'http://localhost:4180/oauth2/callback' \
+  --profile-url 'http://localhost:3100/sso/userinfo'
