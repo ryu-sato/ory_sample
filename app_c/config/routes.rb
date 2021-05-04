f832902b5122da8de095ee8a1964e6675bbdddb2
Ryu Sato <ryu@weseek.co.jp>
2021-05-04 16:18:32 +0900

    app_cのコンテンツを追加

--- app_c/config/routes.rb
+++ app_c/config/routes.rb
@@ -1,3 +1,6 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'news#index'
+
+  resources :news, only: %i[index]
 end
